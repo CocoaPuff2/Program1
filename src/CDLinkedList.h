6496172b89ce605ecec5b4525518978ee07ec88c
Sana (Q) <@CocoaPuff2>
2025-01-07 19:56:23 -0800

    finished CDLinkedList.h

--- src/CDLinkedList.h
+++ src/CDLinkedList.h
@@ -1,8 +1,47 @@
-//
-// Created by Qasim Zuhair on 1/7/25.
-//
 
 #ifndef PROGRAM1_CDLINKEDLIST_H
 #define PROGRAM1_CDLINKEDLIST_H
+
+
+/*
+ * Design and implement a Circular Doubly Linked List with dummy header
+ * with all listed methods with a Node class/struct that holds an int,
+ * and two pointers point to previous and next node.
+ *
+ * Since it is circular and with a dummy header, the first node is a dummy header
+ * and last node's next node should point this dummy header, and dummy header's
+ * previous node is the list's last node.
+ */
+
+struct DlistNode {
+    int item;  // Node class/struct that holds an int
+    DListNode* prev; // two pointers point to previous
+    DListNode* next;  // and next nod
+};
+
+class CDLinkedList {
+    // used the P1 assignment description on canvas as a guide
+public:
+    CDLinkedList();
+    CDLinkedList(const CDLinkedList &rhs)
+    ~CDLinkedList(); // the destructor
+
+    int getCurrentSize() const;
+    bool isEmpty() const;
+
+    bool add(int newEntry);
+    bool remove(int anEntry);
+    void clear();
+
+    virtual bool contains(int anEntry);
+    int getTraverseCount() const;
+    int retrieve(int index);
+    void resetTraverseCount();
+
+
+protected:
+    DListNode *header;  // dummy head node
+
+};
 
 #endif //PROGRAM1_CDLINKEDLIST_H
